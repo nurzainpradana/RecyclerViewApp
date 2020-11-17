be79996d14d298cf098c813099d238ea12dcef4d
nurzainpradana <pradananurzain@gmail.com>
2020-11-17 16:13:29 +0700

    create ListViewModel

--- app/src/main/java/com/example/android/recyclerviewapp/screen/list/ListViewModel.kt
+++ app/src/main/java/com/example/android/recyclerviewapp/screen/list/ListViewModel.kt
@@ -14,4 +14,14 @@ class ListViewModel: ViewModel() {
     init {
         initMenus()
     }
+
+    private fun initMenus() {
+        _menuList = listOf(
+            Menu("Mie Rebus", "Makanan", 15000),
+            Menu("Kopi", "Minuman", 10000),
+            Menu("Bala-Bala", "Gorengan", 2000),
+        )
+
+        _menus.value = _menuList
+    }
 }
